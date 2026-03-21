b5050dcb997e1ef0bf0a62a7d121ce97c849f452
claudioolmedo <109242493+claudioolmedo@users.noreply.github.com>
2026-03-21 14:27:51 -0500

    Fix DOM corruption and force HARD_REFRESH_V2 [v26.03.21.1444]

--- electronics/version.js
+++ electronics/version.js
@@ -1,6 +1,6 @@
 
 window._ODB_VERSION = {
-  tag: "v26.03.21.1425",
-  date: "2026-03-21 14:25:32",
+  tag: "v26.03.21.1427",
+  date: "2026-03-21 14:27:50",
   platform: "One Dollar Board - Electronics Editor"
 };
